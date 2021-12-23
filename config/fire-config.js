be22f9d771aa7dba39b5966ae39cc80f20fce0fe
Alena <83471057+alenamedved@users.noreply.github.com>
2021-12-23 16:12:06 -0700

    initial commit

---
+++ config/fire-config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  };
+
+  try {
+    firebase.initializeApp(firebaseConfig);
+  } catch(err){
+    if (!/already exists/.test(err.message)) {
+      console.error('Firebase initialization error', err.stack)}
+  }
+  const fire = firebase;
+  export default fire;

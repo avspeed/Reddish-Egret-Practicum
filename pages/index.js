0f2fdb65cdd5b16020a44242891f5eeeec453209
Alena <83471057+alenamedved@users.noreply.github.com>
2022-01-13 14:35:21 -0700

    create a hook that returns authUsers

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,32 @@
 import { useState } from "react";
 import Link from "next/link";
 import fire from "../config/fire-config";
-import Layout from "../components/Layout";
+
 import Description from "../components/mainPage/Description";
+import useFirebaseAuth from "../hooks/useFirebaseAuth";
 
 const Home = () => {
+   
   const [loggedIn, setLoggedIn] = useState(false);
   const [notification, setNotification] = useState("");
+  const [userUid, setUserUid] = useState('')
+  const [userEmail, setUserEmail] = useState('')
 
   fire.auth().onAuthStateChanged((user) => {
+   
     if (user) {
       setLoggedIn(true);
+      setUserUid(user.uid)
+      setUserEmail(user.email)
+     
     } else {
       setLoggedIn(false);
     }
+   
   });
 
-  
+console.log(useFirebaseAuth())
+/* console.log(authUser, loading) */
 
   return (
     <>

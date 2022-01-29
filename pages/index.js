dbb0de76d28ca4d6ebea67308ea7beb1716e13c1
Alena <83471057+alenamedved@users.noreply.github.com>
2022-01-29 13:04:35 -0700

    used toasts not notification, if a new user - pushed to profile page, if not - pushed to main board page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 import { useAuth } from "../components/context/authUserContext";
 import Description from "../components/mainPage/Description";
@@ -8,9 +7,11 @@ const Home = () => {
   return (
     <>
       <Description />
-<Link href = {"/mainPageSections/jobs"}passHref>
-     <h3>Jobs and Career</h3>
-     </Link>
+
+      <Link href={"/mainPageSections/jobs"} passHref>
+        <h3>Jobs and Career</h3>
+      </Link>
+      
       <Link href={"/mainPageSections/housingfood"} passHref>
         <h3>Housing And Food</h3>
       </Link>
--- pages/users/join.js
+++ pages/users/join.js
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../../components/context/authUserContext";
+import toast, { Toaster } from "react-hot-toast";
+
+const settings = {
+  loading: 'Saving...',
+  save: <b>Settings saved!</b>,
+  error: <b>Could not save.</b>,
+}
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passConf, setPassConf] = useState("");
-  const [notification, setNotification] = useState("");
 
   const router = useRouter();
 
@@ -17,10 +23,7 @@ const Register = () => {
     //check if passwords match. If they do, create user in Firebase
     // and redirect to mainboard page.
     if (password !== passConf) {
-      setNotification("Password and password confirmation does not match");
-      setTimeout(() => {
-        setNotification("");
-      }, 2000);
+      toast.error("Password and password confirmation does not match");
       setPassword("");
       setPassConf("");
       return null;
@@ -29,22 +32,25 @@ const Register = () => {
     createUserWithEmailAndPassword(email, password)
       .then((authUser) => {
         console.log("Success. The user is created in Firebase");
-        router.push("/profilepage");
+        toast.success("Success!")
+        //if a new user - push to profile page, if not - main board
+        if (authUser.additionalUserInfo.isNewUser) {
+          router.push("/profilepage");
+        } else {
+          router.push("/mainBoard");
+        }
       })
       // An error occurred. Set error message to be displayed to user
       .catch((err) => {
         console.log(err.code, err.message);
-        setNotification(err.message);
-        setTimeout(() => {
-          setNotification("");
-        }, 2000);
+        toast.error(err.message);
       });
   };
-  
+
   return (
     <div>
       <h1>Join our community</h1>
-      {notification}
+      <Toaster />
       <form onSubmit={handleLogin}>
         <label>Email:</label>
         <input

70af7b99e76b0cabedc03c50590d38b07bbcf0b4
Alena <83471057+alenamedved@users.noreply.github.com>
2022-01-30 21:55:04 -0700

    create a 'post' card

--- components/Post.js
+++ components/Post.js
@@ -1,15 +1,98 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import { Box } from "@mui/system";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { autocompleteClasses, Avatar } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { styled } from "@mui/material/styles";
+import Collapse from "@mui/material/Collapse";
+import CardActions from "@mui/material/CardActions";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
-import DeleteIcon from '@mui/icons-material/Delete';
-import { Avatar } from "@mui/material";
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
+const Post = (props) => {
+  const { post } = props;
+  const [expanded, setExpanded] = React.useState(false);
 
- const Post = () => {
-return(
-    <div>
-    <Avatar src=""/>
-    </div>
-)
-}
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  return (
+    <Grid item xs={12} md={6}>
+      <Card>
+        <Box sx={{ display: "flex" }}>
+          {/* <Avatar sx={{ height: "80px", width: "80px" }}> */}
+            <CardMedia
+              component="img"
+              sx={{ width: 120, height: 120, display: { xs: "none", sm: "block" } }}
+              image="../images/treeroots.jpg"
+              alt={"post.imageLabel"}
+            />
+         {/*  </Avatar> */}
+          <CardContent sx={{display: "block" }}>
+            <Typography component="h2" variant="h5">
+              {"user.name"}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              {"post.date"}
+            </Typography>
+            <Typography variant="subtitle1" paragraph sx={{ overflow: "true" }} >
+              {
+                "post.body. bodypost.bodypost.bodypost.body"
+              }
+            </Typography>
+          </CardContent>
+        </Box>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>comments in here</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron
+              and set aside for 10 minutes.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
+  );
+};
 
+/* Post.propTypes = {
+  post: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    imageLabel: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+}; */
 export default Post;
--- pages/mainBoard.js
+++ pages/mainBoard.js
@@ -2,31 +2,27 @@ import React, { useEffect } from "react";
 import { storage, db } from "../config/fire-config";
 import { useRouter } from "next/router";
 import { useAuth } from "../components/context/authUserContext";
-import Post from "../components/Post"
+import Post from "../components/Post";
 
 const MainBoard = () => {
   const { authUser, loading } = useAuth();
   const router = useRouter();
-console.log(loading, authUser)
+  console.log(loading, authUser);
   // Listen for changes on loading and authUser, redirect if needed
- /*  useEffect(() => {
+  /*  useEffect(() => {
     if (!loading && authUser) router.push("/");
   }, [authUser, loading, router]); */
 
-  useEffect( () => {
+  useEffect(() => {
     if (authUser) {
-      console.log('inside here')
-      db.collection("users")
-      .doc(authUser.uid)
-      .get()
-      .then((snapshot) => {
-        const user = snapshot.data();
-        console.log('user', user)
+      console.log("inside here");
+      db.collection("posts").onSnapshot((doc) => {
+        console.log(doc.data());
       });
-            
-    } 
-}, [authUser])
-console.log('mainboard rendered')
+    }
+  }, [authUser]);
+  
+  console.log("mainboard rendered");
   return (
     <>
       <div>You are logged in</div>

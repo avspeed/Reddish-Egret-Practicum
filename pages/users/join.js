c053a1f7e87a819a2b72038ad52cb8109745a975
Alena <83471057+alenamedved@users.noreply.github.com>
2022-01-30 18:59:23 -0700

    style join.js with mui library

--- pages/users/join.js
+++ pages/users/join.js
@@ -1,86 +1,161 @@
-import { useState } from "react";
+import React, {useState} from 'react';
 import { useRouter } from "next/router";
 import { useAuth } from "../../components/context/authUserContext";
 import toast, { Toaster } from "react-hot-toast";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const settings = {
-  loading: 'Saving...',
-  save: <b>Settings saved!</b>,
-  error: <b>Could not save.</b>,
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      
+        UTab App
+      
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-const Register = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passConf, setPassConf] = useState("");
+const theme = createTheme();
 
-  const router = useRouter();
-
-  const { createUserWithEmailAndPassword } = useAuth();
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    //check if passwords match. If they do, create user in Firebase
-    // and redirect to mainboard page.
-    if (password !== passConf) {
-      toast.error("Password and password confirmation does not match");
-      setPassword("");
-      setPassConf("");
-      return null;
-    }
-
-    createUserWithEmailAndPassword(email, password)
-      .then((authUser) => {
-        console.log("Success. The user is created in Firebase");
-        toast.success("Success!")
-        //if a new user - push to profile page, if not - main board
-        if (authUser.additionalUserInfo.isNewUser) {
-          router.push("/profilepage");
-        } else {
-          router.push("/mainBoard");
-        }
-      })
-      // An error occurred. Set error message to be displayed to user
-      .catch((err) => {
-        console.log(err.code, err.message);
-        toast.error(err.message);
-      });
-  };
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passConf, setPassConf] = useState("");
+  
+    const router = useRouter();
+  
+    const { createUserWithEmailAndPassword } = useAuth();
+  
+    const handleRegistration = (e) => {
+      e.preventDefault();
+      //check if passwords match. If they do, create user in Firebase
+      // and redirect to mainboard page.
+      if (password !== passConf) {
+        toast.error("Password and password confirmation does not match");
+        setPassword("");
+        setPassConf("");
+        return null;
+      }
+  
+      createUserWithEmailAndPassword(email, password)
+        .then((authUser) => {
+          console.log("Success. The user is created in Firebase");
+          toast.success("Success!")
+          //if a new user - push to profile page, if not - main board
+          if (authUser.additionalUserInfo.isNewUser) {
+            router.push("/profilepage");
+          } else {
+            router.push("/mainBoard");
+          }
+        })
+        // An error occurred. Set error message to be displayed to user
+        .catch((err) => {
+          console.log(err.code, err.message);
+          toast.error(err.message);
+        });
+    };
 
   return (
-    <div>
-      <h1>Join our community</h1>
-      <Toaster />
-      <form onSubmit={handleLogin}>
-        <label>Email:</label>
-        <input
-          type="text"
-          value={email}
-          onChange={({ target }) => setEmail(target.value)}
-          placeholder="Email"
-          required
-        />
-        <br />
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-          placeholder="Password"
-          required
-        />
-        <br />
-        <label>Password conf:</label>
-        <input
-          type="password"
-          value={passConf}
-          onChange={({ target }) => setPassConf(target.value)}
-          placeholder="Confirm password"
-        />
-        <br />
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <ThemeProvider theme={theme}>
+    <Toaster />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Join Our Community
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleRegistration} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+             <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  value={email}
+                  onChange={({ target }) => setEmail(target.value)}
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  value={password}
+                  onChange={({ target }) => setPassword(target.value)}
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  value={passConf}
+                  onChange={({ target }) => setPassConf(target.value)}
+                  name="password"
+                  label="Confirm Password"
+                  type="password"
+                  id="passwordConfirmation"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/users/login" variant="body2">
+                  Already have an account? Log in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   );
-};
-export default Register;
+}
+
+

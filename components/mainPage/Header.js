db2469508c102f662d4ebfd5d54c4dae9b279c18
tadesse123 <birbirsatadesse@gmail.com>
2022-02-01 00:02:35 -0800

    styling Header

--- components/mainPage/Header.js
+++ components/mainPage/Header.js
@@ -1,34 +1,143 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import fire from "../../config/fire-config";
-import { useAuth } from "../context/authUserContext";
-
-const Header = () => {
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';;
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import { Button, Link} from '@mui/material';
+import { useAuth } from "../context/authUserContext"
+import { ClassNames } from '@emotion/react';
+const  Header = ()=> {
   const { authUser, loading, signOut } = useAuth();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+    className = {ClassNames.humbergermenu}
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Home</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem style = {{backgroundColor: "yellow"}}>
+        
+        <Typography>
+         Home
+        </Typography>
+      </MenuItem>
+      <MenuItem>
+       
+        <p>logout</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen} >
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <header
-      style={{
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-      }}
-    >
-      <Image src="/images/logo.png " width={100} height={100} alt="logo" />
-      <nav>
-        <Link href={"/"}> Home </Link>
-        {!loading && authUser ? (
-          <button onClick={signOut}> Sign out </button>
-        ) : (
-          <div>
-            <Link href={"/users/login"}> Login </Link> |
-            <Link href={"/users/join"}> JoinUs </Link>
-          </div>
-        )}
-      </nav>
-    </header>
-  );
-};
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style = {{backgroundColor: "grey"}}>
+        <Toolbar>
+        <Typography variant = "h4">
+         U Welcome
+        </Typography>
 
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex', } } } style = {{margin:10,  }} >
+          <Link href={"/"}><Button style = {{backgroundColor: "white", margin: 10  }}>Home</Button> </Link>
+          <Link href={"/users/login"}><Button style = {{backgroundColor: "white" , margin: 10 }}>Login</Button> </Link>
+          <Link href={"/users/join"}><Button style = {{backgroundColor: "white", margin: 10   }}>Logout</Button> </Link>
+  
+          </Box>
+          <Box position =  "static" sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
 export default Header;
+
+ 

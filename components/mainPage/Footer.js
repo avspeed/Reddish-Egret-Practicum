d3622638c45e021892ec5a88cefba8c4c338b3f3
tadesse123 <birbirsatadesse@gmail.com>
2022-02-12 23:51:40 -0800

    styling footer

--- components/mainPage/Footer.js
+++ components/mainPage/Footer.js
@@ -1,11 +1,25 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 
 const Footer = () => {
   return (
-    <footer> <small>&copy; 2022 UTab App</small></footer>
+    <Box component="footer" sx={{
+       py: 3,
+    position: "fixed",
+      left: 0,
+      bottom: 0,
+      width: "100%",
+      backgroundColor: "gray",
+      color: "white",
+      textAlign: "center"
+      }}>&copy; 2022 UTab App</Box>
+   
     
   );
 };
 
 export default Footer;
+
+
+ // <footer> <small>&copy; 2022 UTab App</small></footer>

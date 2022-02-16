d3b55aa073a19acd6a59a77b36d2824fbc401ffd
Alena <83471057+alenamedved@users.noreply.github.com>
2022-02-15 21:22:55 -0700

    made logo inside the Header larger

--- components/mainPage/Header.js
+++ components/mainPage/Header.js
@@ -1,9 +1,8 @@
 import React from "react";
-import fire from "../../config/fire-config";
 import { useAuth } from "../context/authUserContext";
 import {
   AppBar,
-  Avatar,
+  CardMedia,
   Box,
   Button,
   IconButton,
@@ -12,7 +11,6 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
-
 const style = {
   display: { xs: "block", sm: "none" },
 };
@@ -20,6 +18,7 @@ const style = {
 const Header = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const { authUser, loading, signOut } = useAuth();
+
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -31,156 +30,155 @@ const Header = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: "#212121" }}>
         <Toolbar>
-          <Avatar
-            alt="Remy Sharp"
-            src="../images/UTab-logos_white.png"
-            sx={{ width: 80, height: 80, marginRight: "auto" }}
+          <CardMedia
+            component="img"
+            alt="app logo"
+            src="../images/UTab-logos_white1.png"
+            sx={{ width: "auto", height: 60, marginRight: "auto" }}
           />
-          <div>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              onClick={handleMenu}
-            >
-              <MenuIcon
-                sx={{
-                  ...style,
-                  "&:hover": {
-                    color: "black",
-                    backgroundColor: "white",
-                  },
-                }}
-              />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
+
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={handleMenu}
+          >
+            <MenuIcon
+              sx={{
+                ...style,
+                "&:hover": {
+                  color: "black",
+                  backgroundColor: "white",
+                },
               }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              {!loading && authUser ? (
-                <div>
-
-
-                  <Button
-                    color="inherit"
-                    sx={{
-                      "&:hover": {
-                        color: "black",
-                        backgroundColor: "white",
-                      },
-                      color: "white",
-                      backgroundColor: "black",
-                      m: 2,
-                    }}
-                    href={"/"}
-                  >
-                    {" "}
-                    Home{" "}
-                  </Button>
-
-                  <Button
-                    color="inherit"
-                    sx={{
-                      "&:hover": {
-                        color: "black",
-                        backgroundColor: "white",
-                      },
-                      color: "white",
-                      backgroundColor: "black",
-                      m: 2,
-                    }}
-                    href={"/mainBoard"}
-                  >
-                    {" "}
-                    Main board{" "}
-                  </Button>
-
-                  <Button
-                    color="inherit"
-                    sx={{
-                      "&:hover": {
-                        color: "black",
-                        backgroundColor: "white",
-                      },
-                      color: "white",
-                      backgroundColor: "black",
-                      m: 2,
-                    }}
-                    onClick={signOut}
-                  >
-                    {" "}
-                    Sign out{" "}
-                  </Button>
-                </div>
-              ) : (
-                <Box>
-                  <Button
-                    sx={{
-                      "&:hover": {
-                        color: "black",
-                        backgroundColor: "white",
-                      },
-                      color: "white",
-                      backgroundColor: "black",
-                      m: 2,
-                    }}
-                    href={"/"}
-                  >
-                    {" "}
-                    Home{" "}
-                  </Button>
-                  <Button
-                    color="inherit"
-                    sx={{
-                      "&:hover": {
-                        color: "black",
-                        backgroundColor: "white",
-                      },
-                      color: "white",
-                      backgroundColor: "black",
-                      m: 2,
-                    }}
-                    href={"/users/login"}
-                  >
-                    {" "}
-                    Login{" "}
-                  </Button>
-                  <Button
-                    color="inherit"
-                    sx={{
-                      "&:hover": {
-                        color: "black",
-                        backgroundColor: "white",
-                      },
-                      color: "white",
-                      backgroundColor: "black",
-                      m: 2,
-                    }}
-                    href={"/users/join"}
-                  >
-                    {" "}
-                    JoinUs{" "}
-                  </Button>
-                </Box>
-              )}
-            </Menu>
-          </div>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
             {!loading && authUser ? (
               <div>
+                <Button
+                  color="inherit"
+                  sx={{
+                    "&:hover": {
+                      color: "black",
+                      backgroundColor: "white",
+                    },
+                    color: "white",
+                    backgroundColor: "black",
+                    m: 2,
+                  }}
+                  href={"/"}
+                >
+                  {" "}
+                  Home{" "}
+                </Button>
+
+                <Button
+                  color="inherit"
+                  sx={{
+                    "&:hover": {
+                      color: "black",
+                      backgroundColor: "white",
+                    },
+                    color: "white",
+                    backgroundColor: "black",
+                    m: 2,
+                  }}
+                  href={"/mainBoard"}
+                >
+                  {" "}
+                  Main board{" "}
+                </Button>
+
+                <Button
+                  color="inherit"
+                  sx={{
+                    "&:hover": {
+                      color: "black",
+                      backgroundColor: "white",
+                    },
+                    color: "white",
+                    backgroundColor: "black",
+                    m: 2,
+                  }}
+                  onClick={signOut}
+                >
+                  {" "}
+                  Sign out{" "}
+                </Button>
+              </div>
+            ) : (
+              <Box>
+                <Button
+                  sx={{
+                    "&:hover": {
+                      color: "black",
+                      backgroundColor: "white",
+                    },
+                    color: "white",
+                    backgroundColor: "black",
+                    m: 2,
+                  }}
+                  href={"/"}
+                >
+                  {" "}
+                  Home{" "}
+                </Button>
+                <Button
+                  color="inherit"
+                  sx={{
+                    "&:hover": {
+                      color: "black",
+                      backgroundColor: "white",
+                    },
+                    color: "white",
+                    backgroundColor: "black",
+                    m: 2,
+                  }}
+                  href={"/users/login"}
+                >
+                  {" "}
+                  Login{" "}
+                </Button>
+                <Button
+                  color="inherit"
+                  sx={{
+                    "&:hover": {
+                      color: "black",
+                      backgroundColor: "white",
+                    },
+                    color: "white",
+                    backgroundColor: "black",
+                    m: 2,
+                  }}
+                  href={"/users/join"}
+                >
+                  {" "}
+                  JoinUs{" "}
+                </Button>
+              </Box>
+            )}
+          </Menu>
+
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {!loading && authUser ? (
+              <>
                 <Button
                   color="inherit"
                   sx={{ backgroundColor: "black", m: 2 }}
@@ -197,7 +195,7 @@ const Header = () => {
                   {" "}
                   Sign out{" "}
                 </Button>
-              </div>
+              </>
             ) : (
               <Box>
                 <Button

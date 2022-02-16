29c9069b3c2f772a813a42c2bb06818b53a3c5fc
Alena <83471057+alenamedved@users.noreply.github.com>
2022-02-15 21:02:50 -0700

    refactored addPost and addnewPost. added top margin to 'create a new post' buttton

--- components/AddnewPost.js
+++ components/AddnewPost.js
@@ -3,20 +3,23 @@ import React, { useState } from "react";
 import { db, timestamp } from "../config/fire-config";
 import { useAuth } from "../components/context/authUserContext";
 
-const AddnewPost = ({ currentUser }) => {
+const AddnewPost = ({ currentUser, handleClose }) => {
   const { authUser } = useAuth();
   const [input, setInput] = useState("");
 
-  const addPost = () => {
-    db.collection("posts").add({
-      postBody: input,
-      createdAt: timestamp,
-      author: authUser.uid,
-      commentCount: 0,
-      likeCount: 0,
-      userName: currentUser.userName,
-      userImageUrl: currentUser.userImageUrl,
-    });
+  const addNewPost = () => {
+      if(input) { //dont send empty string
+          db.collection("posts").add({
+            postBody: input,
+            createdAt: timestamp,
+            author: authUser.uid,
+            commentCount: 0,
+            likeCount: 0,
+            userName: currentUser.userName,
+            userImageUrl: currentUser.userImageUrl,
+          });
+          handleClose()
+      }
     setInput("");
   };
   return (
@@ -39,7 +42,7 @@ const AddnewPost = ({ currentUser }) => {
       <Button
         style={{ position: "absolute", right: "80%", bottom: "30%" }}
         variant="text"
-        onClick={addPost}
+        onClick={addNewPost}
       >
         Submit
       </Button>
--- components/Post.js
+++ components/Post.js
@@ -119,7 +119,7 @@ const Post = ({ post, userId, currentUser }) => {
   };
 
   const dateCreatedAt = new Date(post.createdAt.toDate());
-console.log(post)
+
   return (
     <Grid item xs={6} md={6} sx={{ margin: "10px 0px" }} columns={1}>
       <Card>

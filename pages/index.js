283171ac29a9fc443285f886d7a6bf5dc8512426
Alena <83471057+alenamedved@users.noreply.github.com>
2022-02-16 15:46:39 -0700

    index.js - link removed

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,11 @@
-import { Grid } from "@mui/material";
-import Link from "next/link";
-import { useAuth } from "../components/context/authUserContext";
 import Description from "../components/mainPage/Description";
 import Cardmedia from "../components/Cards/Cardmedia";
 
 const Home = () => {
-
   return (
     <>
       <Description />
       <Cardmedia />
-      <Link href={"/mainPageSections/jobs"} passHref>
-        <h3>Jobs and Career</h3>
-      </Link>
-
-      <Link href={"/mainPageSections/housingfood"} passHref>
-        <h3>Housing And Food</h3>
-      </Link>
-
-      <Link href={"/mainPageSections/educationlanguage"} passHref>
-        <h3>Education and Language</h3>
-      </Link>
     </>
   );
 };

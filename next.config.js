2d92d145ea2067134bca9710db8d4027e2405d29
Team@AVSPEED <support@avspeed.com>
2022-02-22 08:51:16 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     path: "/",
-    domains: ["via.placeholder.com", "firebasestorage.googleapis.com"],
+    domains: ["via.placeholder.com", "firebasestorage.googleapis.com","reddish-egret-practicum-iwbz9ojbg-avspeed.vercel.app"],
   },
 };
